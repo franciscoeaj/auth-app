5ecd9e048b20e5f5de11fb1d59996fe017c20336
Francisco Júnior <franciscojunior@me.com>
2017-05-03 03:50:13 -0300

    controller now fetchs the data from the db

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -3,10 +3,12 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\User;
 
-class UserController
+class UserController extends Controller
 {
     /**
      * @Route("/api/user/authenticate", name="user.authenticate")
@@ -15,12 +17,25 @@ class UserController
     {
         $r = new Response();
 
-        $content = $request->getContent();
-        $json = json_decode($content, true);
-        $email = $json['email'];
-        $password = $json['password'];
+        if ($request->getMethod() == 'POST') {
+            $content = $request->getContent();
+            $json = json_decode($content, true);
+            $email = $json['email'];
+            $password = $json['password'];
+            // $email = $request->request->get('email');
+            // $password = $request->request->get('password');
 
-        $r->setContent(json_encode(['email' => $email, 'password' => $password]));
+            $userRepository = $this->getDoctrine()->getRepository('AppBundle:User');
+            $user = $userRepository->findOneBy(['email' => $email, 'password' => $password]);
+
+            if (is_object($user)) {
+                $r->setContent(json_encode(['id' => $user->getId(), 'name' => $user->getName(), 'email' => $user->getEmail()])); 
+            } else {
+                $r->setContent('This user does not exist');
+            }
+        } else {
+            $r->setContent('Method not allowed');
+        }      
 
         return $r;
     }
